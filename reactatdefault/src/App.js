d47f2deec676f736d7f8ef6c2c50ce96b7236d21
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-05-29 17:52:13 +0530

    learnt about components and also created my first component in .jsx

---
+++ reactatdefault/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+function App() {
+  return (
+    <>
+      <h1>Hello word | Priyanshu Gupta</h1>
+      <Header />
+    </> 
+  );
+}
+
+export default App;
---
+++ reactatdefault/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <h1>Header of my website</h1>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </ul>
+        </>
+    )
+}
+
+export default Header
