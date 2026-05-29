1a0895847a2e5918474f46039b87f4e8f32d57b8
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-05-29 17:48:43 +0530

    successfully unnderstood the react flow and structure and also created components in both the projects

--- react@vite/src/App.jsx
+++ react@vite/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header"
 function App() {
 
   return (
-    <h1>Chai aur react with vite | Priyanshu Gupta</h1>
+    <>
+      <h1>Chai aur react with vite | Priyanshu Gupta</h1>
+      <Header />
+    </>
   )
 }
 
---
+++ react@vite/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <>
+            <div id="Header">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Header
